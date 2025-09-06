4380b8f3b3bcf5db26cf565c13f2fe71ea2cca0f
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-09-07 00:25:17 +0300

    build(core): CORS policy
    Created configuration class with CORS setup

---
+++ src/main/kotlin/org/careerseekers/apientrypoint/config/CorsConfig.kt
@@ -0,0 +1,35 @@
+package org.careerseekers.apientrypoint.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsWebFilter
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
+
+
+@Configuration
+class CorsConfig {
+
+    @Value("\${services.frontend.local}")
+    private lateinit var frontendLocalHost: String
+
+    @Value("\${services.frontend.production}")
+    private lateinit var frontendProductionHost: String
+
+    @Bean
+    fun corsWebFilter(): CorsWebFilter {
+        val corsConfig = CorsConfiguration().apply {
+            addAllowedOrigin(frontendLocalHost)
+            addAllowedOrigin(frontendProductionHost)
+            allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            allowedHeaders = listOf("*")
+            allowCredentials = true
+        }
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", corsConfig)
+
+        return CorsWebFilter(source)
+    }
+}
